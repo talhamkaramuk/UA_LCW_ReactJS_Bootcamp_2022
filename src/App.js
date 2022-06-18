1de2d0182aa197646ddb96a9bf9fedfda0e948c5
talhamkaramuk <tmkaramuk99@gmail.com>
2022-06-18 13:36:28 +0300

    Final commit

--- src/App.js
+++ src/App.js
@@ -6,12 +6,12 @@ import Data from "./components/Data";
 export default class App extends Component {
   state = {
     users: [],
-    done: false,
+    loading: false,
   };
 
   getData = () => {
     axios.get("https://jsonplaceholder.typicode.com/users").then((res) => {
-      this.setState({ users: res.data, done: true });
+      this.setState({ users: res.data, loading: true });
     });
   };
 
@@ -22,7 +22,7 @@ export default class App extends Component {
   render() {
     return (
       <div>
-        {this.state.done ? <Data users={this.state.users} /> : <Loading />}
+        {this.state.loading ? <Data users={this.state.users} /> : <Loading />}
       </div>
     );
   }
